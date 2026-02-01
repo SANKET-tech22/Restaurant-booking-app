d8543161989b344ac173622cc5b59b352838a69b
Sanket-tech22 <sanketpatil08127@gmail.com>
2026-02-01 10:17:23 +0530

    Created docker file for frontend and backend and as well as nginx.conf for reverse proxy

--- frontend/src/Booking.jsx
+++ frontend/src/Booking.jsx
@@ -20,7 +20,7 @@ function Booking() {
     setMessage("⏳ Booking...");
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/book", {
+      const response = await fetch("/book", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
